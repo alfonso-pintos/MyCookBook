e625cf79cde08f93171b9bc48da1e04a0c9c8c1f
Alfonso <alfonso.pintos@icloud.com>
2025-04-21 16:32:51 -0400

    added user input and variable types

---
+++ user_input.py
@@ -0,0 +1,6 @@
+print("Hello!")
+name = input("Please enter your name: ")
+print("Hello " + name)
+
+number = input("Please enter your number: ")
+print(int(number) * 3)
---
+++ variable_types.py
@@ -0,0 +1,7 @@
+my_integer = 3
+my_float = 3.1415
+my_string = "hello"
+
+print(type(my_integer))
+print(type(my_float))
+print(type(my_string))
